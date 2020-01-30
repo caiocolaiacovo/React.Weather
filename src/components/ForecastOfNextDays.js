f6e9b0429d019157cc23da0dcaf76c3a32fdda4a
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2020-01-30 13:07:09 -0400

    WIP - Next days forecast

--- src/components/ForecastOfNextDays.js
+++ src/components/ForecastOfNextDays.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
 
-const ForecastOfNextDays = (props) => (
-  <div></div>
+const ForecastOfNextDays = ({nextDays}) => (
+  <div>
+    Next days:
+    {!!nextDays && nextDays.map(a => (
+      <div>
+        {moment(a.valid_date).format('dddd')}
+        <div>{a.weather.description}</div>
+        <div>Mínima: {a.low_temp}</div>
+        <div>Máxima: {a.max_temp}</div>
+      </div>
+    ))}
+  </div>
 );
 
-export default ForecastOfNextDays;
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    nextDays: state.forecast.nextDays
+  };
+};
+
+export default connect(mapStateToProps)(ForecastOfNextDays);
