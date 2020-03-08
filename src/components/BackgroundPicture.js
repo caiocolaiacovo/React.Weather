eaee0342190684550191ea038cb6d32c7c091856
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2020-03-08 16:11:52 -0400

    wip - background picture component

--- src/components/BackgroundPicture.js
+++ src/components/BackgroundPicture.js
@@ -9,12 +9,13 @@ const thunderstormCodes = {
   231: 'thundestorm',
   232: 'thundestorm',
   233: 'thundestorm',
+  
 };
 
 const BackgroundPicture = ({code, partOfTheDay}) => {
   return (
     <div className='background-picture' style={{
-      backgroundImage: `url("/photos/thunderstorm_${code === 'd' ? 'day' : 'night'}.jpg")`
+      backgroundImage: `url("/photos/thunderstorm_${partOfTheDay === 'd' ? 'day' : 'night'}.jpg")`
       }}></div>
   );
 };
