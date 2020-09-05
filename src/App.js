8029e3ad248509c9d02b346487c5803bd339bfa6
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2020-09-05 16:49:25 -0400

    Adding blank slate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Search from './components/Search';
 import Weather from './components/Weather';
 import Forecast from './components/Forecast';
 import BackgroundPicture from './components/BackgroundPicture';
 import './App.css';
+import BlankSlate from './components/BlankSlate';
 
-function App() {
+function App({weather, forecast, loading}) {
+  const showWeather = Object.keys(weather).length !== 0;
+  const showForecast = forecast.today.length !== 0 && forecast.nextDays.length !== 0;
+  
   return (
     <div className='app'>
       <BackgroundPicture />
       <Search/>
-      <Weather/>
-      <Forecast/>
+      {showWeather && <Weather/>}
+      {showForecast && <Forecast/>}
+      {(!showWeather || !showForecast) && <BlankSlate/>}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => state;
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/BlankSlate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BlankSlate() {
+  return (
+    <div className="blank-slate">
+      <div className="blank-slate__container">
+        <img alt="blank slate" src="/blankslate.png"/>
+        <span>type your city or location to get weather conditions</span>
+      </div>
+    </div>
+  );
+};
+
+export default BlankSlate;
